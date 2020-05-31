6e89eb63da3af1aeed320572b7473d19a456343f
채원찬 <opace2341@kookmin.ac.kr>
2020-05-31 20:32:41 +0900

    기본세팅

--- main.cpp
+++ main.cpp
@@ -3,4 +3,16 @@ using namespace std;
 
 int main(){
     cout << "Hello\n";
+    int num = 4;
+    int numOfMinterm = 6;
+    int numOfDoncare = 2;
+    int sumOfNum = numOfDoncare+numOfMinterm;
+    int minterms[sumOfNum];
+    int table[num][num];
+    int tmp;
+    for(int i = 0; i<sumOfNum; i++){
+        cin >> tmp;
+        minterms[i] = tmp; 
+    }
+    
 }
